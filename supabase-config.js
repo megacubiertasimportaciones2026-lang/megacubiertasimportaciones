f3130db7a580d8e6358e34ab3fc3a1aedc85ac58
megacubiertasimportaciones2026-lang <megacubiertasimportaciones2026@gmail.com>
2026-05-26 02:24:24 -0500

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.MC_SUPABASE_CONFIG = {
+  url: "https://lolxwmudymyyulkqbfls.supabase.co",
+  anonKey: "sb_publishable_kJAWjdizLAZKITpG_g8w1A_dPzsTHqt",
+  table: "site_content",
+  siteId: "megacubiertas",
+  bucket: "megacubiertas-images"
+};
